f98eedd9c4ff8a35e024b3b5fed540946cc09c5f
Ben Brown <33357404+BenBrownCreative@users.noreply.github.com>
2022-06-24 13:47:07 -0500

    noImplicitAny changed to true

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -2,7 +2,10 @@ import { apiCall } from '../utils/networking';
 
 const cachedMessageRecordArrays: Record<string, any> = {};
 
-export async function getChannelMessages(teamId, channelId) {
+export async function getChannelMessages(
+  teamId: any,
+  channelId: any,
+) {
   let cached = cachedMessageRecordArrays[channelId];
   if (typeof cached === 'undefined')
     cached = cachedMessageRecordArrays[channelId] = apiCall(
--- src/ui/components/SelectedChannel.tsx
+++ src/ui/components/SelectedChannel.tsx
@@ -13,7 +13,7 @@ const SelectedChannel: React.FunctionComponent<any> = ({
   const { channelId: selectedChannelId } = params;
   if (!selectedChannelId) return <p>Invalid channelId</p>;
   const selectedChannel = channels.find(
-    (c) => c.id === selectedChannelId,
+    (c: any) => c.id === selectedChannelId,
   );
   if (!selectedChannel)
     return (
